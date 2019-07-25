1791d7559a0bdd92687543e7842b48c66a75c649
Nima <nmoosavi@live.com>
2019-07-25 17:25:30 +0430

    ENTITY MODEL

--- src/main/java/ir/webold/liveHesab/model/UserInformation.java
+++ src/main/java/ir/webold/liveHesab/model/UserInformation.java
@@ -2,11 +2,11 @@ package ir.webold.liveHesab.model;
 
 import ir.webold.liveHesab.base.BaseEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Table(name = "SEC_USERINFORMATION")
 public class UserInformation extends BaseEntity<UUID> {
 
     @Column(name = "FIRSTNAME" ,length = 20)
@@ -14,6 +14,32 @@ public class UserInformation extends BaseEntity<UUID> {
 
     @Column(name = "LASTNAME",length = 20)
     private String lastName;
+
+    @Column(name = "ISSUENUMBER" ,length = 12)
+    private String issueNumber;
+
+    @Column(name = "IDNUMBER" ,length = 12)
+    private String idNumber;
+
+    @Column(name = "TELL" ,length = 15)
+    private String tell;
+
+    @Column(name = "MOBILE" ,length = 15)
+    private String mobile;
+
+    @Column(name = "ADRESS")
+    private String adress;
+
+    @Column(name = "POSTALCODE" ,length = 15)
+    private String postalCode;
+
+    @Column(name = "EMAIL" ,length = 50)
+    private String email;
+
+
+    @JoinColumn(name = "SEC_USERINFORMATION", referencedColumnName = "SEC_USERS")
+    @Column(name = "SEC_USERS_FK")
+    private User user;
 
 
 }
